2bf1e367d866647e2adec100716d7c15c17f0341
Susumu Fujii <powermac.g7@gmail.com>
2024-11-29 12:31:23 +0900

    feat: rename apps/app to apps/web

--- apps/app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@sujii/portofolio-ui/components/ui/Button";
-
-export default function Home() {
-  return (
-    <main className="p-24">
-      <Button>aaa</Button>
-    </main>
-  );
-}
